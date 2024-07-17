3d464443720bdfaf7a688a852c4daf5bb335bfc6
asrorovuz <asrorovsardor07@gmail.com>
2024-07-17 10:11:29 +0500

    add loading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import About from "./about/about";
 import BestLocation from "./best-location/best-location";
 import Contact from "./contact/contact";
@@ -7,25 +8,42 @@ import Navbar from "./navbar/navbar";
 import Reservation from "./reservation/reservation";
 import SupportVisa from "./support-visa/support-visa";
 import Tours from "./tours/tours";
+import loadingImg from "../assets/loading.svg";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 2000);
+  }, [])
+
   return (
     <>
-      <header className="relative min-h-[70px]">
-        <Navbar />
-      </header>
-      <main>
-        <Home />
-        <Tours />
-        <SupportVisa/>
-        <About />
-        <BestLocation/>
-        <Reservation/>
-        <Contact />
-      </main>
-      <footer>
-        <Footer/>
-      </footer>
+      {loading ? (
+        <>
+          <header className="relative min-h-[70px]">
+            <Navbar />
+          </header>
+          <main>
+            <Home />
+            <Tours />
+            <SupportVisa />
+            <About />
+            <BestLocation />
+            <Reservation />
+            <Contact />
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </>
+      ) : (
+        <div className="w-full h-[100vh]">
+          <img className="w-full h-full" src={loadingImg} alt="loading" />
+        </div>
+      )}
     </>
   );
 }
